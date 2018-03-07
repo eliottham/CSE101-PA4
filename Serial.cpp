44bb8400f26e169d32525ce64c5494a1f8a2fcae
Eliott H Ham <cs101web@ieng6-202.ucsd.edu>
2018-03-06 18:03:57 -0800

    Finished Serial.cpp

--- Serial.cpp
+++ Serial.cpp
@@ -1,16 +1,33 @@
 // CSE 101 Winter 2018, PA 4
 //
-// Name: TODO put both partners' info if applicable
-// PID: TODO
-// Sources of Help: TODO
+// Name: Eliott Ham
+// PID: A13186685
+// Sources of Help: Piazza
 // Due: March 10th, 2018 at 11:59 PM
 
 #ifndef __SERIAL_CPP__
 #define __SERIAL_CPP__
 
 #include "Serial.hpp"
+using namespace std;
 
 int serial(std::vector<int> nums) {
-  return 0;
+  int half = nums.size() / 2;
+
+  int start = nums[0];
+  int mid = start + half;
+
+  if(nums.size() == 1) {
+  	return nums[0];
+  }
+
+  if(nums[half] < mid) {
+  	vector<int> first_half(nums.begin(), nums.begin() + half);
+  	return serial(first_half);
+  }
+  else {
+  	vector<int> second_half(nums.begin() + half, nums.end());
+  	return serial(second_half);
+  }
 }
 #endif
